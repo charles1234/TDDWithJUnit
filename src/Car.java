987074be14710e4793d47491ab19d373a49c1130
charles1234 <captainfalcon530@gmail.com>
2016-01-29 16:08:51 -0600

    Implemented cannotDecelerate Method()

--- src/Car.java
+++ src/Car.java
@@ -45,7 +45,12 @@ public class Car {
 	}
 	
 	public void decelerate(int amount) {
-		speed -= amount;
+		if (speed - amount < 0) {
+			speed = 0;
+		} else {
+			speed-= amount;
+			
+		}
 	}
 	
 	/**
